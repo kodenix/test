9280f00601ae64022b3f225d748ffa64e6b0a710
Alberto <kodenix@gmail.com>
2015-04-23 18:30:41 +0200

    cambios

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ $webAuth = new dbx\WebAuthNoRedirect($appInfo, "PHP-Example/1.0");
 $authorizeUrl = $webAuth->start();
 
 echo "1. Go to: " . $authorizeUrl . "\n";
-/*echo "2. Click \"Allow\" (you might have to log in first).\n";
+echo "2. Click \"Allow\" (you might have to log in first).\n";
 echo "3. Copy the authorization code.\n";
-$authCode = \trim(\readline("Enter the authorization code here: "));
+$authCode = \trim(\readline("YVn3nrDJlBEAAAAAAAAGksz7E7w9gK-OSXaMeWQImLc"));
 
 list($accessToken, $dropboxUserId) = $webAuth->finish($authCode);
 print "Access Token: " . $accessToken . "\n";
@@ -35,4 +35,3 @@ $fileMetadata = $dbxClient->getFile("/working-draft.txt", $f);
 fclose($f);
 print_r($fileMetadata);
 
-*/
