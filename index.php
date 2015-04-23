380f1d3e3a5ee3e2c1428e8863af52b84ea899e8
Alberto <kodenix@gmail.com>
2015-04-23 17:05:51 +0200

    config the repo

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "dropbox-sdk/Dropbox/autoload.php";
+use \Dropbox as dbx;
+
+
+$appInfo = dbx\AppInfo::loadFromJsonFile("app-info.json");
+$webAuth = new dbx\WebAuthNoRedirect($appInfo, "PHP-Example/1.0");
+
+echo PHP_INT_MAX;
+
